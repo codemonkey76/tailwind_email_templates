48e14570b4285c1ab0e24591a46b50cc6fc44be9
Shane Poppleton <shane@bjja.com.au>
2024-04-14 17:32:16 +1000

    Added template for password resets

--- inline.js
+++ inline.js
@@ -1,9 +1,44 @@
 // inline.js
 import fs from "fs";
+import path from "path";
 import juice from "juice";
 
-const html = fs.readFileSync("./src/template.html", "utf8");
-const css = fs.readFileSync("./src/output.css", "utf8");
-const inlinedHtml = juice.inlineContent(html, css);
+const src = "./src";
+const dst = "./dist";
 
-fs.writeFileSync("./public/inlined_template.html", inlinedHtml);
+if (!fs.existsSync(dst)) {
+  fs.mkdirSync(dst, { recursive: true });
+}
+
+fs.readdir(src, (err, files) => {
+  if (err) {
+    return console.error(`Failed to read directory: ${src}: ${err.message}`);
+  }
+
+  files.forEach((file) => {
+    if (path.extname(file) === ".html") {
+      const srcFilePath = path.join(src, file);
+      const dstFilePath = path.join(dst, file);
+
+      fs.readFile(srcFilePath, "utf8", (err, data) => {
+        if (err) {
+          return console.error(
+            `Failed to read file: ${srcFilePath}: ${err.message}`,
+          );
+        }
+
+        const inlinedHtml = juice(data);
+
+        fs.writeFile(dstFilePath, inlinedHtml, "utf8", (err) => {
+          if (err) {
+            return console.error(
+              `Failed to write file: ${dstFilePath}: ${err.message}`,
+            );
+          } else {
+            console.log(`Successfully inlined CSS in: ${dstFilePath}`);
+          }
+        });
+      });
+    }
+  });
+});
