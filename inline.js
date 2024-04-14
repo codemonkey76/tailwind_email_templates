c7131e2200b389aa95f6d7ee66ef653107ed828a
Shane Poppleton <shane@bjja.com.au>
2024-04-15 07:49:23 +1000

    Work on password reset email

--- inline.js
+++ inline.js
@@ -1,6 +1,7 @@
 // inline.js
 import fs from "fs";
 import path from "path";
+//import inlineCss from "inline-css";
 import juice from "juice";
 
 const src = "./src";
@@ -14,20 +15,27 @@ fs.readdir(src, (err, files) => {
   if (err) {
     return console.error(`Failed to read directory: ${src}: ${err.message}`);
   }
+  const css = fs.readFileSync("./src/output.css", "utf8");
 
   files.forEach((file) => {
     if (path.extname(file) === ".html") {
       const srcFilePath = path.join(src, file);
       const dstFilePath = path.join(dst, file);
 
-      fs.readFile(srcFilePath, "utf8", (err, data) => {
+      fs.readFile(srcFilePath, "utf8", async (err, data) => {
         if (err) {
           return console.error(
             `Failed to read file: ${srcFilePath}: ${err.message}`,
           );
         }
 
-        const inlinedHtml = juice(data);
+        /*        const inlinedHtml = await inlineCss(data, {
+          url: " ",
+          applyStyleTags: true,
+          removeStyleTags: false,
+        }); */
+
+        const inlinedHtml = juice.inlineContent(data, css);
 
         fs.writeFile(dstFilePath, inlinedHtml, "utf8", (err) => {
           if (err) {
